6eaa05957ae6c7d56e0264a6f02800c00b6c833a
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2023-09-05 20:15:49 +0700

    Update Dijkstra.cpp

--- 1/Dijkstra.cpp
+++ 1/Dijkstra.cpp
@@ -1,1 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <climits>
 
+using namespace std;
+
+const int V = 6; // Number of vertices in the graph
+
+// Function to find the vertex with the minimum distance value
+int minDistance(const vector<int>& dist, const vector<bool>& visited) {
+    int minDist = INT_MAX;
+    int minIndex = -1;
+
+    for (int v = 0; v < V; ++v) {
+        if (!visited[v] && dist[v] < minDist) {
+            minDist = dist[v];
+            minIndex = v;
+        }
+    }
+
+    return minIndex;
+}
+
+// Function to print the shortest path from the source to all vertices
+void printShortestPath(const vector<int>& dist) {
+    cout << "Vertex \t Distance from Source\n";
+    for (int i = 0; i < V; ++i) {
+        cout << i << "\t" << dist[i] << endl;
+    }
+}
+
+// Function to implement Dijkstra's algorithm to find the shortest path
+void dijkstra(const vector<vector<int>>& graph, int src) {
+    vector<int> dist(V, INT_MAX); // Initialize distances to infinity
+    vector<bool> visited(V, false); // Initialize visited array
+
+    dist[src] = 0; // Distance to source is 0
+
+    for (int count = 0; count < V - 1; ++count) {
+        int u = minDistance(dist, visited);
+        visited[u] = true;
+
+        for (int v = 0; v < V; ++v) {
+            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    // Print the shortest path distances
+    printShortestPath(dist);
+}
+
+int main() {
+    // Example graph represented as an adjacency matrix
+    vector<vector<int>> graph = {
+        {0, 2, 0, 0, 3, 0},
+        {2, 0, 4, 0, 0, 1},
+        {0, 4, 0, 1, 0, 0},
+        {0, 0, 1, 0, 7, 0},
+        {3, 0, 0, 7, 0, 5},
+        {0, 1, 0, 0, 5, 0}
+    };
+
+    int source = 0; // Source vertex
+
+    dijkstra(graph, source); // Find and print shortest path
+
+    return 0;
+}
